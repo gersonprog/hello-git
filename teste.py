007fadc371c0301a8969d2be6b6f11e9f6cfb601
Gerson Sant'anaE <ger@a.comm>
2023-05-03 21:35:08 -0300

    traduçaõ do texto

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-nome = input("difite seu nome")
-print("Boa noite!")
-print("Seja bem vindo " + nome)
-print("Obrigado por visitar nosso site.")
+nome = input("Type your name")
+print("Good night!")
+print("WELCOME " + nome)
+print("Thanks!")
